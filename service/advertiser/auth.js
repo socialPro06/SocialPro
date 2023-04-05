05aa12a5f839c7b0f9af201d45c79f1a4c26240d
ishika <ishikav.tagline@gmail.com>
2023-04-05 20:35:22 +0530

    Admin user Manage approve & Block API  2

--- service/advertiser/auth.js
+++ service/advertiser/auth.js
@@ -3,24 +3,23 @@ const jwt = require('jsonwebtoken')
 const { encrypt } = require('../../helper/encrypt-decrypt')
 const commonService = require('../common')
 const otpModel = require('../../model/otp')
-const blockUserModel = require("../../model/blockUser")
 
 module.exports = {
-    register: (data) => {
-        return new Promise(async (res, rej) => {
-            try {
-                let newAdvertiserModel = new advertiserModel(data)
-                let saveData = newAdvertiserModel.save()
-                if (saveData) {
-                    res({ status: 200, data: "Advertiser register successfull...!!" })
-                } else {
-                    rej({ status: 404, message: "Something Went Wrong...!!" })
-                }
-            } catch (err) {
-                rej({ status: err?.status || 500, error: err, message: err?.message || "Something Went Wrong...!!" })
+register : (data)=>{
+    return new Promise(async (res,rej)=>{
+        try {
+            let newAdvertiserModel = new advertiserModel(data)
+            let saveData = newAdvertiserModel.save()
+            if (saveData) {
+                res({status:200 , data:"Advertiser register successfull...!!"})
+            } else {
+                rej({status:404,message:"Something Went Wrong...!!"})
             }
-        })
-    },
+        } catch (err) {
+            rej({status:err?.status || 500 , error:err,message:err?.message ||"Something Went Wrong...!!" })
+        }
+    })
+},
 
 login:(emailId , password)=>{
     return new Promise(async (res,rej)=>{
@@ -31,6 +30,8 @@ login:(emailId , password)=>{
                 let encryptAdvertiser = encrypt(loginData._id,key1)
                 let encryptEmail = encrypt(loginData.emailId , key1)
                 let encryptPass = encrypt(loginData.password,key1)
+                
+
                 let token = jwt.sign({
                     advertiserId : encryptAdvertiser,
                     emailId : encryptEmail,
@@ -49,126 +50,67 @@ login:(emailId , password)=>{
                 res({status:200 , data : data})
             } else {
                 rej({status:404, message:"Email or password incorrect !!"})
-=======
-                    let token = jwt.sign({
-                        advertiserId: encryptAdvertiser,
-                        emailId: encryptEmail,
-                        password: encryptPass
-                    },
-                        process.env.ADVERTISER_ACCESS_TOKEN,
-                        { expiresIn: process.env.ADVERTISER_ACCESS_TIME }
-                    )
-
-                    let data = {
-                        token: token,
-                        firstName: loginData.firstName,
-                        lastName: loginData.lastName
-                    };
-                    res({ status: 200, data: data })
-                } else {
-                    rej({ status: 404, message: "Email or password incorrect !!" })
-                }
-            } catch (err) {
-                rej({ status: 500, error: err, message: 'Advertiser login something went wrong!!' });
             }
-        })
-    },
+        } catch (err) {
+            rej({ status: 500, error: err, message:'Advertiser login something went wrong!!'});
+        }
+    })
+},
 
-    forgot: (emailId, otp, newPassword, confirmPassword) => {
-        return new Promise(async (res, rej) => {
-            try {
-                let resp = await commonService.commonOtp(emailId)
-                let otpData = await otpModel.findOne({ emailId, otp })
-                if (otpData) {
-                    if (newPassword == confirmPassword) {
+forgot: (emailId,otp,newPassword,confirmPassword) => {
+    return new Promise(async (res, rej) => {
+        try {
+            let resp = await commonService.commonOtp(emailId)
+            let otpData = await otpModel.findOne({emailId , otp})
+            if (otpData) {
+                if (newPassword == confirmPassword) {
                         let getData1 = await advertiserModel.findOneAndUpdate({ emailId: emailId }, { password: newPassword, confirmPassword: newPassword }, { new: true })
                         if (getData1) {
+                            res({status:200,data:""})
+                        } else {
+                            rej({status:404,message:"Password cannot be changed...!!"})
+                        }
+                } else {
+                    rej({ status: 404, message: "newPassword and confirmPassword not match" })
+                }
+            } else {
+                rej({ status: 404, message: "Email and otp incorrect..!!" })
+            }
+        } catch (err) {
+            rej({ status: 500, error: err, message: "Advertiser forGot password something went wrong!!" });
+        }
+    })
+},
+
+changePass: (data, emailId) => {
+    return new Promise(async (res, rej) => {
+        try {
+            if (emailId == data.emailId) {
+                let cpassword = data.confirmPassword;
+                if (data.password == cpassword) {
+                    let getData1 = await advertiserModel.findOne({ emailId: data.emailId });
+                    console.log(getData1)
+                    if (getData1) {
+                        let getData = await advertiserModel.findOneAndUpdate({ emailId: data.emailId }, { password: data.password, confirmPassword: data.password }, { new: true })
+                        // console.log("getData ......................", getData);
+                        if (getData) {
                             res({ status: 200, data: "" })
                         } else {
-                            rej({ status: 404, message: "Password cannot be changed...!!" })
+                            rej({ status: 404, message: "Password can't Updated..!!" })
                         }
                     } else {
-                        rej({ status: 404, message: "newPassword and confirmPassword not match" })
+                        rej({ status: 404, message: "Email incorrect" })
                     }
                 } else {
-                    rej({ status: 404, message: "Email and otp incorrect..!!" })
+                    rej({ status: 404, error: "Password and Confirm Password not match ...!!" })
                 }
-            } catch (err) {
-                rej({ status: 500, error: err, message: "Advertiser forGot password something went wrong!!" });
+            } else {
+                rej({ status: 404, error: "Please enter right Email...!!", message: "Something went Wrong.." })
             }
-        })
-    },
-
-    changePass: (data, emailId) => {
-        return new Promise(async (res, rej) => {
-            try {
-                if (emailId == data.emailId) {
-                    let cpassword = data.confirmPassword;
-                    if (data.password == cpassword) {
-                        let getData1 = await advertiserModel.findOne({ emailId: data.emailId });
-                        console.log(getData1)
-                        if (getData1) {
-                            let getData = await advertiserModel.findOneAndUpdate({ emailId: data.emailId }, { password: data.password, confirmPassword: data.password }, { new: true })
-                            // console.log("getData ......................", getData);
-                            if (getData) {
-                                res({ status: 200, data: "" })
-                            } else {
-                                rej({ status: 404, message: "Password can't Updated..!!" })
-                            }
-                        } else {
-                            rej({ status: 404, message: "Email incorrect" })
-                        }
-                    } else {
-                        rej({ status: 404, error: "Password and Confirm Password not match ...!!" })
-                    }
-                } else {
-                    rej({ status: 404, error: "Please enter right Email...!!", message: "Something went Wrong.." })
-                }
-            } catch (err) {
-                console.log("Error....", err)
-                rej({ status: 500, error: err, message: "Something went Wrong.." })
-            }
-        })
-    },
-
-    checkEmail: (emailId) => {
-        return new Promise(async (res, rej) => {
-            try {
-                let email = await advertiserModel.findOne({ emailId });
-                if (email) {
-                    rej({ status: 404, message: "Email Already Exist !!" })
-                } else {
-                    let blockEmail = await blockUserModel.findOne({ emailId });
-                    if (blockEmail) {
-                        rej({ status: 404, message: "Email is Blocked !!" })
-                    } else {
-                        res({ status: 200, data: email });
-                    }
-                }
-            } catch (error) {
-                rej({ status: 500, error: err, message: 'Something went wrong!!' });
-            }
-        })
-    },
-
-    checkMobile: (mobileNo) => {
-        return new Promise(async (res, rej) => {
-            try {
-                let mobileNO = await advertiserModel.findOne({ mobileNo });
-                if (mobileNO) {
-                    rej({ status: 400, message: "Mobile no is already exist." });
-                } else {
-                    let blockedMobile = await blockUserModel.findOne({ mobileNo });
-                    if (blockedMobile) {
-                        rej({ status: 400, message: "Mobile no is Blocked." });
-                    } else {
-                        res({ status: 200, data: mobileNO });
-                    }
-                }
-            } catch (err) {
-                rej({ status: 500, error: err, message: 'Something went wrong!!' });
-            }
-        })
-    }
-
+        } catch (err) {
+            console.log("Error....", err)
+            rej({ status: 500, error: err, message: "Something went Wrong.." })
+        }
+    })
+}
 }
